8eac7f1588ce3ad0593a086916003b3d74441d1a
Gregory-Jagermeister <38982561+Gregory-Jagermeister@users.noreply.github.com>
2020-04-28 19:44:43 +1000

    Added the article driver
    on page load the webpage will load all the requried information from the json file. first test may need more work

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 
-fetch("./articles.json")
+import * as views from "/views.js"
+
+function windowLoad() {
+    fetch("./articles.json")
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        
+        console.log(data)
+        views.displayArticle(document.getElementById("templateinsert"), data)
     })
 
-window.onload = function(){
-
 }
+
+window.onload = windowLoad;
---
+++ js/views.js
@@ -0,0 +1,15 @@
+
+export { displayArticle }
+
+function applyTemplate(targetid, templateid, data) {
+    let target = document.getElementById(targetid);
+
+    let template = Handlebars.compile(
+                        document.getElementById(templateid).textContent
+                    )
+    target.innerHTML = template(data);
+}
+
+function displayArticle(targetid, data) {
+    applyTemplate(targetid, "article-content", {articles :data})
+}
