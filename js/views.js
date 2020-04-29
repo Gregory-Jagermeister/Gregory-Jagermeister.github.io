64f770bc25eee9b70e9a07fe4a5407990924c40f
Gregory-Jagermeister <38982561+Gregory-Jagermeister@users.noreply.github.com>
2020-04-29 16:30:19 +1000

    Test

--- js/views.js
+++ js/views.js
@@ -6,7 +6,7 @@ function applyTemplate(targetid, templateid, data) {
 
     let template = Handlebars.compile(document.getElementById(templateid).textContent)
     console.log(data)
-    target.innerHTML = template(data);
+    target.innerHTML = "gay";
 }
 
 function displayArticle(targetid, data) {
