a7e1ec2c4001ace522fdc3c83213d06e67c3a263
Gregory-Jagermeister <38982561+Gregory-Jagermeister@users.noreply.github.com>
2020-04-29 16:22:43 +1000

    Update views.js

--- js/views.js
+++ js/views.js
@@ -4,9 +4,8 @@ export { displayArticle }
 function applyTemplate(targetid, templateid, data) {
     let target = document.getElementById(targetid);
 
-    let template = Handlebars.compile(
-                        document.getElementById(templateid).textContent
-                    )
+    let template = Handlebars.compile(document.getElementById(templateid).textContent)
+    console.log(data)
     target.innerHTML = template(data);
 }
 
